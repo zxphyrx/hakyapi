9025fe6655bc73b260860bc0a36f1ba85703fefd
Zeph Requilme <zephrequilme40@gmail.com>
2025-10-06 10:10:48 +0800

    Made the scraper

---
+++ scraper.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import fs from "fs";
+
+async function scrape() {
+    let { data } = await axios.get("https://haikyuu.fandom.com/wiki/Characters");
+    let $ = cheerio.load(data);
+    const characterLinks = [];
+    const finalData = [];
+
+    $(".wikia-gallery-item > .lightbox-caption > a").each((_, elem) => {
+    characterLinks.push($(elem).attr("href"));
+    })
+  
+    async function getData() {
+        await Promise.all(characterLinks.map(async (link) => {
+            let { data } = await axios.get(`https://haikyuu.fandom.com${link}`);
+            let $ = cheerio.load(data);
+            let name = $(`[data-source="title"]`).text();
+            let characterData = {};
+
+            characterData.name = name;
+
+            $("br").replaceWith("\n");
+            $(`[class="pi-item pi-data pi-item-spacing pi-border-color"]`).each((_, elem) => {
+                let key = $(elem).attr("data-source").replaceAll(" ", "-");
+                let value = $(elem).find(`[class="pi-data-value pi-font"]`).text();
+                characterData[key] = value;
+            })
+
+            finalData.push(characterData);
+        }))
+    }
+
+    await getData();
+    fs.writeFileSync("data.json", JSON.stringify(finalData, null, 2));
+}
+
+scrape();
